b84f6d77c4a7dd02ba38a282ed9bdc1613db5dc5
clm110 <275298711@qq.com>
2018-05-23 07:34:20 +0800

    完善爬虫

--- app5.js
+++ app5.js
@@ -21,52 +21,25 @@ let options = {
 };
 
 makeDir('test').then(path => {
-    // console.log(path);
     testPath = path;
-    
 }).then(()=>{
     rp(options).then(($) => {
         book.title = $("#info h1").text();
         let domarr = $('#list dd a');
         domarr.each((index, ele) => {
-            var obj = {
-                title: $(ele).text(),
-                url: `http://www.biquke.com/bq/0/990/${$(ele).attr('href')}`
-            }
-            arr.push(obj);
-            arr = arr.filter((ele, index) => {
-                if (ele.title.indexOf("第") == 0) {
-                    return true;
-                } else {
-                    return false
-                }
-            })
+            var url =  `http://www.biquke.com/bq/0/990/${$(ele).attr('href')}`
+            arr.push(url);
         });
-        var uriarr = arr.map((ele) => {
-            return {
-                uri: ele.url
-            }
-        });
-        return Promise.resolve(uriarr)
-    }).then(function(data){
-        let urls = data.map((e)=>e.uri)
+        return Promise.resolve(arr)
+    }).then(function(urls){
         loop(urls)
     })
 });
-
-
-
-
-
 
 function loop(bigarr){
     let arr = bigarr.splice(0,20)
     if(arr.length>0){
       async.mapLimit(arr,10,function(url,cb){
-        // rq(url,(err,data)=>{
-        //     let $ = cheerio.load(data.body)
-        //     cb(null,$('title').text())
-        // })
         rqpage(url,cb)
       },function(err,results){
         loop(bigarr)
@@ -84,11 +57,15 @@ function rqpage(url, cb) {
         }
     }).then(($)=>{
         let content = $('#content').html();
-        let title = $('title').text()
-        let abctestPath = path.join(testPath, `/${title}.html`);
+        let title = $('title').text().replace("_笔趣阁","")
+        let pre = $(".bookname a").eq(1).attr('href');
+        let next = $(".bookname a").eq(3).attr('href');
+        let abctestPath = path.join(testPath, path.basename(url));
         let contentHtml = template(templatepath,{
-            title:title,
-            content:content
+            title,
+            content,
+            pre,
+            next
         })
         cb(null, $('title').text());
         // console.log(Buffer.alloc(content).toString())
