cd5132a78eb2831cb308c9ad0585c661f4bce6f0
clm110 <275298711@qq.com>
2018-05-25 16:43:27 +0800

    增加首页代理爬虫

--- app6.js
+++ app6.js
@@ -1,8 +1,7 @@
-const cheerio = require('cheerio');
 const path = require('path');
 const template = require('art-template')
 const express = require('express');
-const axios = require('axios');
+const router = require('./router/router.js')
 const app = express()
 
 
@@ -11,54 +10,9 @@ app.engine('html', require('express-art-template'));
 app.set('view engine', "html")
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/book/:num/:book', (req, res) => {
-    let num = req.params.num;
-    let book = req.params.book;
-    let url = `http://www.biquke.com/bq/${num}/${book}/`
-    axios.get(url).then(function (result) {
-        let arr =  [];
-        let $ = cheerio.load(result.data);
-        let domarr = $('#list dd a');
-        let latest = $('#info p a[href]').eq(0).text();
-        let title = $('#info h1').text()
-        let href = $('#info p a[href]').eq(0).attr('href');
-        domarr.each((index, ele) => {
-            let obj = {}
-            obj.url = $(ele).attr('href')
-            obj.title = $(ele).text();
-            arr.push(obj);
-        });
-        // let str =  template(path.join(__dirname,'./views/index.html'),{href:href,latest:latest,arr:arr})
-        // res.send(str);
-        res.render('index', {
-            href: href,
-            latest: latest,
-            arr: arr,
-            title
-        })
-    })
-})
+app.use(router);
 
-app.get('/page/:num/:book/:page', (req, res) => {
-    let page = req.params.page
-    let book = req.params.book;
-    let num = req.params.num
-    let url = `http://www.biquke.com/bq/${num}/${book}/${page}`
-    axios.get(url).then((result) => {
-        let $ = cheerio.load(result.data);
-        let content = $('#content').html();
-        let title = $('title').text().replace("_笔趣阁", "")
-        let pre = $(".bookname a").eq(1).attr('href');
-        let next = $(".bookname a").eq(3).attr('href');
-        res.render('template', {
-            content,
-            title,
-            pre,
-            next
-        });
-    })
-})
 
 app.listen(3001, () => {
-    console.log("http://127.0.0.1:3001")
+    console.log("http://127.0.0.1:3001/0/990")
 })
---
+++ router/router.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+
+
+router.get('/book/:num/:book', (req, res) => {
+    let num = req.params.num;
+    let book = req.params.book;
+    let url = `http://www.biquke.com/bq/${num}/${book}/`
+    axios.get(url).then(function (result) {
+        let arr =  [];
+        let $ = cheerio.load(result.data);
+        let domarr = $('#list dd a');
+        let latest = $('#info p a[href]').eq(0).text();
+        let title = $('#info h1').text()
+        let href = $('#info p a[href]').eq(0).attr('href');
+        domarr.each((index, ele) => {
+            let obj = {}
+            obj.url = $(ele).attr('href')
+            obj.title = $(ele).text();
+            arr.push(obj);
+        });
+        res.render('book', {
+            href: href,
+            latest: latest,
+            arr: arr,
+            title,
+            num,
+            book
+        })
+    })
+})
+
+router.get('/page/:num/:book/:page', (req, res) => {
+    let page = req.params.page
+    let book = req.params.book;
+    let num = req.params.num
+    let url = `http://www.biquke.com/bq/${num}/${book}/${page}`
+    axios.get(url).then((result) => {
+        let $ = cheerio.load(result.data);
+        let content = $('#content').html();
+        let title = $('title').text().replace("_笔趣阁", "")
+        let pre = $(".bookname a").eq(1).attr('href');
+        let next = $(".bookname a").eq(3).attr('href');
+        let bookHome = `/book/${num}/${book}`
+        res.render('template', {
+            content,
+            title,
+            pre,
+            next,
+            bookHome
+        });
+    })
+})
+
+router.get('/',function(req,res){
+    let newscontent = [];
+    axios.get('http://www.biquke.com').then((result)=>{
+        let $ = cheerio.load(result.data);
+         $("#newscontent .l li").each(function(i,e){
+            newscontent.push({
+                category:$(e).has('.s1').text(),
+                bookTilte:$(e).has('.s2').find('a').text(),
+                bookHref:$(e).has('.s2').find('a').attr("href"),
+                pageTilte:$(e).find('.s3').find('a').text(),
+                pageHref:$(e).find('.s3').find('a').attr("href"),
+                author:$(e).find('.s4').text(),
+                time:$(e).find('.s5').text()
+            })
+        })
+        res.json({newscontent})      
+        // let newscontent = $("#newscontent .l li").map(function(i,e){
+        //     return {
+        //         a:"2222"
+        //         // category:$(e).has('.s1').text(),
+        //         // bookTilte:$(e).has('.s2').find('a').text(),
+        //         // bookHref:$(e).has('.s2').find('a').attr("href"),
+        //         // pageTilte:$(e).find('.s3').find('a').text(),
+        //         // pageHref:$(e).find('.s3').find('a').attr("href"),
+        //         // author:$(e).find('.s4').text(),
+        //         // time:$(e).find('.s5').text()
+        //     }
+        // })
+
+    })
+})
+
+module.exports = router;
